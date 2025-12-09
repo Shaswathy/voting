5d05b152dd83b207c797498e417b019fafa9473d
Shaswathy <shaswathylakshmi@gmail.com>
2025-12-09 20:37:41 +0530

    Delete PollApp/src/main/java/com/ty/pollapp/repository directory

--- PollApp/src/main/java/com/ty/pollapp/repository/PollRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ty.pollapp.repository;
-
-import com.ty.pollapp.entity.Poll;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PollRepository extends JpaRepository<Poll, Long> {
-}
--- PollApp/src/main/java/com/ty/pollapp/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ty.pollapp.repository;
-
-import com.ty.pollapp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
